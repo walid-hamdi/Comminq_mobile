bc03c365bbdf429fec4ea095e7fb8d8b316cf495
walid hamdi <walidhamdidev@gmail.com>
2023-06-12 11:59:32 +0100

    Add environment.dart

---
+++ lib/environment.dart
@@ -0,0 +1,6 @@
+class Environment {
+  static const endPoint = String.fromEnvironment('ENDPOINT');
+  static const clientId = String.fromEnvironment('CLIENT_ID');
+  static const sentryDsn = String.fromEnvironment('SENTRY_DSN');
+}
+
